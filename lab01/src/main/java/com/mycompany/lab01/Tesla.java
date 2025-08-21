767daf6b54eafb464ebf0a423b63b28ef2e954a8
Angelo2024-cb <canoangelo993@gmai.com>
2025-08-21 04:19:27 -0500

    Agregando carpeta de laboratorio01

---
+++ lab01/src/main/java/com/mycompany/lab01/Tesla.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.lab01;
+
+/**
+ *
+ * @author hp
+ */
+public class Tesla extends VehiculoElectrico {
+    private boolean autopilot;
+
+    public Tesla(String marca, String modelo, int año, double precio, double capacidadBateria, double autonomia, boolean autopilot) {
+        super(marca, modelo, año, precio, capacidadBateria, autonomia);
+        this.autopilot = autopilot;
+    }
+
+    public void activarModoEco() {
+        System.out.println("Modo Eco activado en el Tesla.");
+    }
+
+    public void mostrarInfo() {
+        System.out.println("Marca: " + marca + ", Modelo: " + modelo + ", Año: " + año + ", Precio: " + precio);
+        System.out.println("Capacidad batería: " + capacidadBateria + " kWh, Autonomía: " + autonomia + " km, Autopilot: " + (autopilot ? "Sí" : "No"));
+    }
+
+    public void activarAutopilot() {
+        if (autopilot) {
+            System.out.println("Autopilot activado.");
+        } else {
+            System.out.println("Este Tesla no tiene Autopilot.");
+        }
+    }
+}
