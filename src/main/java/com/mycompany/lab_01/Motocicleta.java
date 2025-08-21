c0e73063ebaaaac6e8d08ddcc9e3089c69947015
Angelo2024-cb <canoangelo993@gmai.com>
2025-08-21 03:03:00 -0500

    Laboratorio 01

---
+++ src/main/java/com/mycompany/lab_01/Motocicleta.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.lab_01;
+
+/**
+ *
+ * @author hp
+ */
+public class Motocicleta extends Vehiculo {
+    private int cilindrada;
+    private boolean tieneSidecar;
+
+    public Motocicleta(String marca, String modelo, int año, double precio, int cilindrada, boolean tieneSidecar) {
+        super(marca, modelo, año, precio);
+        this.cilindrada = cilindrada;
+        this.tieneSidecar = tieneSidecar;
+    }
+
+    public void mostrarInfo() {
+        super.mostrarInfo();
+        System.out.println("Cilindrada: " + cilindrada + "cc, Sidecar: " + (tieneSidecar ? "Sí" : "No"));
+    }
+
+    public void hacerCaballito() {
+        System.out.println("¡Haciendo caballito!");
+    }
+}
